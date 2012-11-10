60e33fd145eb47eb8050cdf4b9875696b178c38c
Adrian <adrianooo231@gmail.com>
2012-11-10 21:59:15 +0000

    First Commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+//
+//  main.cpp
+//  Ipad Managment LIbrary 2012
+//
+//  Created by Adrian Maciaszek on 10/11/2012.
+//  Copyright (c) 2012 Larkin Community College. All rights reserved.
+//
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+
+using namespace std;
+
+void addIpad()
+{
+    
+}
+
+void removeIpad()
+{
+    
+}
+
+int main(int nNumberofArgs, char* pszArgs[])
+{
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
